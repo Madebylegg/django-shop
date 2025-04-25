594b853567e5f5aa9b8c090f937622044117b23c
Madebylegg <madebylegg@gmail.com>
2025-04-25 11:22:21 +0100

    Add temporary superuser creation on first startup in production

--- config/settings.py
+++ config/settings.py
@@ -138,3 +138,17 @@ SOCIALACCOUNT_LOGIN_ON_GET = True
 SOCIALACCOUNT_QUERY_EMAIL = True
 SOCIALACCOUNT_EMAIL_VERIFICATION = "optional"
 SOCIALACCOUNT_EMAIL_REQUIRED = True
+
+
+
+# --- TEMP: Create superuser on first startup in production ---
+if os.environ.get("RENDER") and os.environ.get("DJANGO_SUPERUSER_EMAIL"):
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(email=os.environ["DJANGO_SUPERUSER_EMAIL"]).exists():
+        User.objects.create_superuser(
+            username=os.environ["DJANGO_SUPERUSER_USERNAME"],
+            email=os.environ["DJANGO_SUPERUSER_EMAIL"],
+            password=os.environ["DJANGO_SUPERUSER_PASSWORD"]
+        )
+# --- END TEMP ---
